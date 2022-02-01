ede493bcf1f0a7a12edfa9a92ee2345afe01f410
jediemil <emil.reyier.hedin@gmail.com>
2022-02-01 10:23:34 +0100

    menuchange

--- js/bord.js
+++ js/bord.js
@@ -1,14 +1,20 @@
-const width = 10
-const height = 3
+const width = 7
+const height = 5
+const max = 31
 
 for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
         let element = document.createElement("button");
-        element.innerText = "Hej"
-        element.classList.add("timeButton")
+        element.innerText = "Mars " + ((x + (y*width)) + 1)
         document.getElementById("timeTable").insertBefore(element, document.getElementById("placeholder"));
         console.log(element)
+
+        if ((x + (y * width) + 1) >= max) {
+            break
+        }
     }
 }
+
+document.getElementById("back").style.display = "none"
 
 document.getElementById("placeholder").remove()
