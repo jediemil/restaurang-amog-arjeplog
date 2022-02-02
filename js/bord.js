30b08cd77d6af749a3c8cfd833120bb9ef4fed64
jediemil <emil.reyier.hedin@gmail.com>
2022-02-02 13:55:08 +0100

    bord

--- js/bord.js
+++ js/bord.js
@@ -5,16 +5,17 @@ const max = 31
 for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
         const day = ((x + (y * width)) + 1)
+
+        if (day-1 >= max) {
+            break
+        }
+
         let element = document.createElement("button");
         element.innerText = "Mars " + day
         document.getElementById("timeTable").insertBefore(element, document.getElementById("placeholder"));
         console.log(element)
         element.onclick = function () {
             
-        }
-
-        if (day >= max) {
-            break
         }
     }
 }
